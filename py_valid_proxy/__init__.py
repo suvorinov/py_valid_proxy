dfc3743b678406ed136e6abb2b4ba87131c3d863
Suvorinov Oleg <suvorinovoleg@yandex.ru>
2024-04-05 10:22:59 +0300

    Изменена версия пакета

--- py_valid_proxy/__init__.py
+++ py_valid_proxy/__init__.py
@@ -2,11 +2,11 @@
 # @Author: Suvorinov Oleg
 # @Date:   2023-11-13 10:16:09
 # @Last Modified by:   Suvorinov Oleg
-# @Last Modified time: 2024-04-04 18:10:20
+# @Last Modified time: 2024-04-05 10:22:23
 
 __author__ = 'Oleg Suvorinov'
 __email__ = 'suvorinovoleg@yandex.ru'
-__version__ = '0.2.9'
+__version__ = '0.3.0'
 
 
 from .valid_proxy import valid_proxy_ip
